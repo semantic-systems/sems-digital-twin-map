dec2bb3933a11bd0e5fd8b2803f3699e7c04507b
David Rath <davidrath828@gmail.com>
2023-11-03 13:07:33 +0100

    Add Feature, Style to model.py, removed Point/Region

--- model.py
+++ model.py
@@ -1,60 +1,47 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float, JSON
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float, JSON, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from geoalchemy2 import Geometry
 
 Base = declarative_base()
 
+class Feature(Base):
+    __tablename__ = 'features'
+    id = Column(Integer, primary_key=True)
+    properties = Column(JSON)
+    geometry_type = Column(String, nullable=False)
+    geometry = Column(Geometry(geometry_type='GEOMETRY'), nullable=False)
+    feature_set_id = Column(Integer, ForeignKey('feature_sets.id'))
+    feature_set = relationship('FeatureSet', back_populates='features')
+
 class FeatureSet(Base):
     __tablename__ = 'feature_sets'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    color = Column(String)
-    fill_color = Column(String)
-    colormap_id = Column(Integer, ForeignKey('colormaps.id'))
-    colormap = relationship('Colormap', back_populates='featureSet', uselist=False)
-    popup_properties = Column(JSON)
-    features = relationship('Feature', back_populates='featureSet')
+    name = Column(String, nullable=False)
+    style_id = Column(Integer, ForeignKey('styles.id'), nullable=True)
+    style = relationship('Style', back_populates='feature_sets')
+    features = relationship('Feature', back_populates='feature_set')
 
 class Colormap(Base):
     __tablename__ = 'colormaps'
     id = Column(Integer, primary_key=True)
-    featureSet = relationship('FeatureSet', back_populates='colormap', uselist=False)
-    property = Column(String)
-    min_value = Column(Float)
-    max_value = Column(Float)
-    min_color = Column(String)
-    max_color = Column(String)
+    property = Column(String, nullable=False)
+    min_value = Column(Float, nullable=False)
+    max_value = Column(Float, nullable=False)
+    min_color = Column(String, nullable=False)
+    max_color = Column(String, nullable=False)
+    styles = relationship('Style', back_populates='colormap')
 
-class Feature(Base):
-    __tablename__ = 'features'
+class Style(Base):
+    __tablename__ = 'styles'
     id = Column(Integer, primary_key=True)
-    feature_set_id = Column(Integer, ForeignKey('feature_sets.id'))
-    properties = Column(JSON)
-    type = Column(String)
-    featureSet = relationship('FeatureSet', back_populates='features')
-    __mapper_args__ = {
-        'polymorphic_on': type,
-        'polymorphic_identity': 'feature'
-    }
-
-class Point(Feature):
-    __tablename__ = 'points'
-    id = Column(Integer, ForeignKey('features.id'), primary_key=True)
+    name = Column(String, nullable=False)
+    popup_properties = Column(JSON)
+    color = Column(String)
+    fill_color = Column(String)
     icon_prefix = Column(String)
     icon_name = Column(String)
-    geometry_type = Column(String)  # Always 'POINT'
-    geometry = Column(Geometry(geometry_type='POINT'))
-    __mapper_args__ = {
-        'polymorphic_identity': 'point'
-    }
-
-class Region(Feature):
-    __tablename__ = 'regions'
-    id = Column(Integer, ForeignKey('features.id'), primary_key=True)
-    geometry_type = Column(String)
-    geometry = Column(Geometry(geometry_type='GEOMETRY'))
     line_weight = Column(Float)
-    __mapper_args__ = {
-        'polymorphic_identity': 'region'
-    }
+    colormap_id = Column(Integer, ForeignKey('colormaps.id'), nullable=True)
+    colormap = relationship('Colormap', back_populates='styles')
+    feature_sets = relationship('FeatureSet', back_populates='style')
