18dc8df234a8c6c5e34de143fffd83b04aa5f8e3
cedricmoller <cedric.moeller@icloud.com>
2025-09-29 16:12:59 +0200

    Add updating the database with reports from the KG

--- src/server_reports.py
+++ src/server_reports.py
@@ -19,7 +19,7 @@ if not SPARQL_ENDPOINT:
     raise ValueError("SPARQL_ENDPOINT environment variable is not set.")
 
 # how long to wait between requests (in seconds)
-REQUEST_DELAY = 60 * 60 * 6    # 6 hours
+REQUEST_DELAY = 10
 
 # how long to wait before timing out a request (in seconds)
 TIMEOUT_DELAY = 300            # 5 minutes
@@ -57,39 +57,68 @@ SEARCH_LOOK_BACK = 10    # how many minutes to look back
 
 sparql = SPARQLWrapper(SPARQL_ENDPOINT)
 
+def get_keycloak_token():
+    KEYCLOAK_URL = "https://node-1.net.uhh.rescue-mate.de/auth"
+    REALM = "master"
+    CLIENT_ID = "uhh"
+
+    USERNAME = os.getenv('USERNAME', '')
+    PASSWORD = os.getenv('PASSWORD', '')
+
+    token_url = f"{KEYCLOAK_URL}/realms/{REALM}/protocol/openid-connect/token"
+
+    data = {
+        "grant_type": "password",
+        "client_id": CLIENT_ID,
+        "username": USERNAME,
+        "password": PASSWORD,
+        # Optional, falls dein Client ein Secret verlangt:
+        # "client_secret": "dein-secret",
+    }
+
+    response = requests.post(token_url, data=data)
+    response.raise_for_status()
+    token = response.json()["access_token"]
+
+    return token
+
 
 def fetch_social_media_posts(search_since: datetime):
     """Fetch posts from RescueMate KG."""
 
+    authorization_headers = {"Authorization": f"Bearer {get_keycloak_token()}"}
+
     search_since_str = search_since.isoformat()
 
     query = f"""
-    PREFIX sm: <http://social_media_data.com/>
+    PREFIX rm: <http://rescue-mate.de/resource/>
+    PREFIX schema: <http://schema.org/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         SELECT * {{
-        ?post a sm:Post ;
-        ?post sm:text ?text ;
-        ?post sm:created_at ?date ;
-        ?post sm:predicted_category ?category ;
-        ?post sm:platform ?platform ;
-        ?post sm:mentioned_location ?location_mention ;
-        ?post sm:url ?url .
-        ?location_mention sm:location ?location ;
-        ?location sm:osm_type ?osm_type ;
-        ?location sm:osm_id ?osm_id ;
-        ?location sm:latitude ?lat ;
-        ?location sm:longitude ?lon ;
-        ?location sm:name ?name ;
-        ?location sm:display_name ?display_name ;
-        ?location_mention sm:text ?mention .
-        
+        ?post a rm:SocialMediaPost ;
+        schema:text ?text ;
+        schema:dateCreated ?date ;
+        rm:hasDetectedCategory ?category ;
+        rm:hasMentionedLocation ?location_mention ;
+        schema:author ?user .
+        OPTIONAL {{ ?post rm:predictedRelevance ?relevance .}}
+        ?user rm:socialMediaServiceName ?platform .
+        ?location_mention rm:location ?location ;
+        schema:text ?location_mention_surface_form .
+        ?location rm:osm_type ?osm_type ;
+        rm:osm_id ?osm_id ;
+        rm:latitude ?lat ;
+        rm:longitude ?lon ;
+        rdfs:label ?name .
         FILTER (?date > "{search_since_str}"^^xsd:dateTime)
         }}
     """
 
     sparql.setQuery(query)
     sparql.setReturnFormat('json')
-    sparql.setMethod('POST')
-    sparql.addCustomHttpHeader('User-Agent', USER_AGENT)
+    sparql.setMethod('GET')
+    sparql.addCustomHttpHeader("User-Agent", USER_AGENT)
+    sparql.addCustomHttpHeader("Authorization", authorization_headers["Authorization"])
     try:
         results = sparql.query().convert()
     except Exception as e:
@@ -107,30 +136,29 @@ def fetch_social_media_posts(search_since: datetime):
                 'text': result['text']['value'],
                 'timestamp': result['date']['value'],
                 'platform': result['platform']['value'].split('/')[-1],
-                'url': result['url']['value'],
+                'url': result.get('url', {"value": ""})['value'],
                 'event_type': result.get('category', {}).get('value', 'other'),
+                'relevance': result.get('relevance', {}).get('value', 'unknown'),
                 'geo_linked_entities': [{
-                    'mention': result['mention']['value'],
+                    'mention': result['location_mention_surface_form']['value'],
                     'location': {
                         'osm_type': result['osm_type']['value'],
                         'osm_id': int(result['osm_id']['value']),
                         'lat': float(result['lat']['value']),
                         'lon': float(result['lon']['value']),
                         'name': result['name']['value'],
-                        'display_name': result['display_name']['value']
                     }
                 }]}
         else:
             posts[post_id]['geo_linked_entities'].append(
                 {
-                    'mention': result['mention']['value'],
+                    'mention': result['location_mention_surface_form']['value'],
                     'location': {
                         'osm_type': result['osm_type']['value'],
                         'osm_id': int(result['osm_id']['value']),
                         'lat': float(result['lat']['value']),
                         'lon': float(result['lon']['value']),
                         'name': result['name']['value'],
-                        'display_name': result['display_name']['value']
                     }
                 }
             )
@@ -305,12 +333,17 @@ if __name__ == '__main__':
 
     # an initial sleep, because the api might not be ready yet
     print(f'Waiting for the API to be ready. Sleeping for {TIMEOUT_DELAY} seconds')
-    time.sleep(30)
-
-    search_since = datetime.now() - timedelta(minutes=SEARCH_LOOK_BACK)
+    #time.sleep(30)
+    start_date = datetime.now()
+    search_since = start_date - timedelta(minutes=SEARCH_LOOK_BACK)
     while True:
         new_search_since = datetime.now()
-        posts = fetch_social_media_posts(search_since)
+        try:
+            posts = fetch_social_media_posts(search_since)
+        except:
+            print("Error fetching posts, retrying in next cycle")
+            posts = []
+            time.sleep(10)
         search_since = new_search_since
 
         for post in posts:
